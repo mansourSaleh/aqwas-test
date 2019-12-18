b302a17e48291deb14c3bbef9f6afc892973cfe1
mansourSaleh <mansouralmohsen2018@gmai.com>
2019-12-18 23:23:59 +0300

    add landing page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./custom.scss";
+// import 'bootstrap/dist/css/bootstrap.min.css';
+import { Button, Container, Row, Col } from "react-bootstrap";
+// import "bootswatch/dist/minty/bootstrap.min.css";
+import "./App.css";
+import bg from "./images/map.png";
+import { IconHome, Setting } from "./icons";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <img src={bg} className="bg" />
+      <section className="page-wrap">
+        <Container>
+          <Row>
+            <Col>
+            <IconHome />
+            </Col>
+
+          </Row>
+          
+        {/* <section className="justify"> */}
+
+          <Row className="justify">
+            <Col xs={2} lg={1} className="marginH">
+              <Setting />
+            </Col>
+           
+            
+           <Col xs={6} lg={3} >
+
+              <Button style={{borderRadius: 10}} block  className="myButton" color="primary">
+                اقتراح
+              </Button>
+           </Col>
+           
+            </Row>
+        {/* </section> */}
+        </Container>
+      </section>
     </div>
   );
 }
