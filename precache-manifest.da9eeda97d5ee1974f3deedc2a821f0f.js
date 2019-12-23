9c89236c4709a04cabd7d4d6e8bcd21fc418dbf6
mansourSaleh <mansouralmohsen2018@gmai.com>
2019-12-23 17:17:32 +0300

    Updates

---
+++ precache-manifest.da9eeda97d5ee1974f3deedc2a821f0f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "669da2e4607900d1812d28e7607d60b3",
+    "url": "/aqwas-test/index.html"
+  },
+  {
+    "revision": "2235f9d2ce840d3f4f5f",
+    "url": "/aqwas-test/static/css/main.446dcc86.chunk.css"
+  },
+  {
+    "revision": "183817ab874a3c128aa9",
+    "url": "/aqwas-test/static/js/2.a409614b.chunk.js"
+  },
+  {
+    "revision": "2f7d7f97110d0b96584e71a5b2872c8b",
+    "url": "/aqwas-test/static/js/2.a409614b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2235f9d2ce840d3f4f5f",
+    "url": "/aqwas-test/static/js/main.14acafe7.chunk.js"
+  },
+  {
+    "revision": "0086393bd0c13de18a39",
+    "url": "/aqwas-test/static/js/runtime-main.c8fde2e2.js"
+  },
+  {
+    "revision": "ef197752ceef2c7eee605f0b67bc1e5e",
+    "url": "/aqwas-test/static/media/error.ef197752.jpg"
+  },
+  {
+    "revision": "9894060a4c4e7f5adde82638f43ab737",
+    "url": "/aqwas-test/static/media/map.9894060a.png"
+  }
+]);
